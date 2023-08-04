2dbfca2b1403a02ef266cae6791340610bddecef
Vova-iz-Tambova <voller.ruden@yandex.ru>
2023-08-04 03:35:15 +0300

    rebuild trash like

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,13 @@
+import React from 'react'
+import CurrentUserContext from '../contexts/CurrentUserContext'
+
 function Card(props) {
+  const currentUser = React.useContext(CurrentUserContext)
+  const isOwn = props.card.owner._id === currentUser._id
+  const isLiked = props.card.likes.some(i => i._id === currentUser._id)
+  const cardLikeButtonClassName = (
+    `element__like ${isLiked && 'element__like_active'}`
+  )
 
   function handleClick() {
     props.onCardClick(props.card)
@@ -9,10 +18,10 @@ function Card(props) {
       <div style={{ backgroundImage: `url(${props.card.link})` }} className="element__link" onClick={handleClick} />
       <h2 className="element__name">{props.card.name}</h2>
       <div className="element__likes">
-        <button className="element__like" type="button"></button>
+        <button className={cardLikeButtonClassName} type="button"></button>
         {props.card.likes.length}
       </div>
-      <button className="element__delete-button  link-effect" type="button"></button>
+      {isOwn && <button className="element__delete-button  link-effect" type="button" />}
     </div>
   )
 }
